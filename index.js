38b5dfb607b46faffd391dcf07430c6bd9d4779a
Joshua Perry <josh@6bit.com>
2022-03-13 14:15:39 -0600

    Add basic express server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("."));
+
+app.listen(5000, () => {
+  console.log("server listening on http://localhost:5000");
+});
